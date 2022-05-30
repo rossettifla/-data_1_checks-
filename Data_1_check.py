5777db7aa38bce81c969d8b541c6b851bdb79a1a
flavia <rossettifla@gmail.com>
2022-05-30 12:50:32 -0400

    knowledge check 1

---
+++ Data_1_check.py
@@ -0,0 +1,7 @@
+print('Hello world')
+animals = ['cat', 'dog', 'bird', 30, 30.5]
+print(animals[1])
+animals_dic = {'cat': 2, 'dog':4}
+print(animals_dic['cat'])
+tuple_value = (10, 20, 30, 40)
+print(tuple_value[2])
